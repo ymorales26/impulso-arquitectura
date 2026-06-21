695f96068c616b87303e678c24d7f388cec0a4f5
vi18xd <jenni_jc9@hotmail.com>
2026-06-21 09:10:29 -0500

    chore: configuracion de despliegue para github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // O el plugin de react que tengas instalado
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/impulso-arquitectura/', // <--- AGREGA ESTA LÍNEA EXACTA CON LAS BARRAS
 })
