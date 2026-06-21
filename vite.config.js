10ed4fed30391498f7b77f81445c61499cc11ec8
vi18xd <jenni_jc9@hotmail.com>
2026-06-21 09:24:12 -0500

    chore: quitar base de github pages para vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react' // O el plugin de react que tengas inst
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/impulso-arquitectura/', // <--- AGREGA ESTA LÍNEA EXACTA CON LAS BARRAS
+  //base: '/impulso-arquitectura/', // <--- AGREGA ESTA LÍNEA EXACTA CON LAS BARRAS
 })
